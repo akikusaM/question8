9563dd2647ed03ed39174b2ac0562bf1dcdb006c
akikusa <akikusa.t-engineers@tsuchiya-gousei.com>
2023-09-29 13:49:48 +0900

    first

---
+++ todo-project/src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+// TodoListコンポーネントをimport
+import TodoList from '../views/TodoList.vue'
+import IssueList from '../views/IssueList.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  // '/todos'パス追加
+  {
+    path: '/todo-list',
+    name: 'TodoList',
+    component: TodoList
+  },
+  // '/todos'パス追加
+  {
+    path: '/issue-list',
+    name: 'IssueList',
+    component: IssueList
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
